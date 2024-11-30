8cd4caadb6de81cd8f74079426359a7a02ef7ed3
semmlerb <semmlerb@mcmaster.ca>
2024-11-30 02:31:53 -0500

    Iteration 2A Completed

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -1,5 +1,78 @@
 #include "objPosArrayList.h"
 
-// Paste your Tested implementation here.
-// Paste your Tested implementation here.
-// Paste your Tested implementation here.
+// Check lecture contents on general purpose array list construction, 
+// and modify it to support objPos array list construction.
+
+objPosArrayList::objPosArrayList() //initializing our arrayList
+{
+    arrayCapacity = ARRAY_MAX_CAP;
+    listSize = 0;
+
+    aList = new objPos[arrayCapacity];
+    for(int i = 0; i<arrayCapacity; i++){
+        aList[i] = objPos();
+    }
+}
+
+objPosArrayList::~objPosArrayList()
+{
+    delete aList;
+}
+
+int objPosArrayList::getSize() const
+{
+    return listSize;
+}
+
+
+void objPosArrayList::insertHead(objPos thisPos)
+{
+    listSize++;
+
+    objPos temp = thisPos;
+    objPos prevVal;
+
+    for(int i = 0; i<listSize; i++){
+        prevVal = aList[i];
+        aList[i] = temp;
+        temp = prevVal;
+    }
+    
+    
+    
+
+}
+
+void objPosArrayList::insertTail(objPos thisPos)
+{
+    aList[listSize] = thisPos;
+    listSize++;
+}
+
+void objPosArrayList::removeHead()
+{
+    listSize--;
+    for(int i = 0; i<listSize; i++){
+        aList[i] = aList[i+1];
+    }
+}
+
+void objPosArrayList::removeTail()
+{
+    listSize--;
+}
+
+objPos objPosArrayList::getHeadElement() const
+{
+    return aList[0];
+}
+
+objPos objPosArrayList::getTailElement() const
+{
+    return aList[(listSize-1)];
+}
+
+objPos objPosArrayList::getElement(int index) const
+{
+    return aList[index];
+}
